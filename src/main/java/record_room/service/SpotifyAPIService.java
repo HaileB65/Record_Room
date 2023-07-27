94eb14b7b9f7c8821682ef3f2638f2cb3374833a
HaileB65 <92234955+HaileB65@users.noreply.github.com>
2023-07-27 16:43:19 -0500

    Finished getAccessToken method that now returns spotify api token.

--- src/main/java/record_room/service/SpotifyAPIService.java
+++ src/main/java/record_room/service/SpotifyAPIService.java
@@ -1,17 +1,20 @@
 package record_room.service;
 
 import org.springframework.stereotype.Service;
-import record_room.models.SpotifyAPIResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.URLEncoder;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
+import java.util.HashMap;
+import java.util.stream.Collectors;
 
 @Service
 public class SpotifyAPIService {
@@ -24,22 +27,31 @@ public class SpotifyAPIService {
     @Autowired
     RestTemplate restTemplate;
 
-    public SpotifyAPIResponse getAccessToken(){
-        String clientCredentials = "2a2a74173789459097512d4d6189a074"+ ":" + "d5559a3ad12b4b66ae1c9001310b1c87";
-        String encodedCredentials = Base64.getEncoder().encodeToString(clientCredentials.getBytes(StandardCharsets.UTF_8));
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.valueOf(MediaType.APPLICATION_FORM_URLENCODED_VALUE));
-        headers.set("Authorization", "Basic " + encodedCredentials);
-
-        String body = "grant_type=client_credentials";
-
-        HttpEntity<String> request = new HttpEntity<>(body, headers);
-
+    public String getAccessToken() throws IOException, InterruptedException {
+        String keys = "2a2a74173789459097512d4d6189a074:d5559a3ad12b4b66ae1c9001310b1c87";
+//        String encodedCredentials = Base64.getEncoder().encodeToString(clientCredentials.getBytes(StandardCharsets.UTF_8));
         String url = "https://accounts.spotify.com/api/token";
 
-        ResponseEntity<SpotifyAPIResponse> response = restTemplate.postForEntity(url, request, SpotifyAPIResponse.class);
-        System.out.println("test");
-        return response.getBody();
+        HashMap<String, String> parameters = new HashMap<>();
+        parameters.put("grant_type", "client_credentials");
+        String form = parameters.keySet().stream()
+                .map(key -> key + "=" + URLEncoder.encode(parameters.get(key), StandardCharsets.UTF_8))
+                .collect(Collectors.joining("&"));
+        String encoding = Base64.getEncoder().encodeToString(keys.getBytes());
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(url))
+                .headers("Content-Type", "application/x-www-form-urlencoded", "Authorization", "Basic "+encoding)
+                .POST(HttpRequest.BodyPublishers.ofString(form)).build();
+        HttpResponse<?> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.statusCode() + response.body().toString());
+
+//        HttpHeaders headers = new HttpHeaders();
+//        headers.setContentType(MediaType.valueOf(MediaType.APPLICATION_FORM_URLENCODED_VALUE));
+//        headers.set("Authorization", "Basic MmEyYTc0MTczNzg5NDU5MDk3NTEyZDRkNjE4OWEwNzQ6ZDU1NTlhM2FkMTJiNGI2NmFlMWM5MDAxMzEwYjFjODc=");
+//        String body = "grant_type=client_credentials";
+//        HttpEntity<String> request = new HttpEntity<>(body, headers);
+//        ResponseEntity<SpotifyAPIResponse> response = restTemplate.postForEntity(url, request, SpotifyAPIResponse.class);
+//        System.out.println("test");
+        return response.body().toString();
     }
 }
