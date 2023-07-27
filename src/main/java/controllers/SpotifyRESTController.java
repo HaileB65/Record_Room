343c995f4de4fd3acad3d10533f99cabaa9778a8
HaileB65 <92234955+HaileB65@users.noreply.github.com>
2023-07-26 20:18:29 -0500

    Created SpotifyRESTController.

--- src/main/java/controllers/SpotifyRESTController.java
+++ src/main/java/controllers/SpotifyRESTController.java
@@ -1,12 +1,12 @@
 package controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SpotifyRESTController {
 
-    @GetMapping()
+    @PostMapping()
     public void getAccessToken(){
 
     }
