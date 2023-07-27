dc458df17339aeba20cdb0a0d3aa5fe67167d42b
HaileB65 <92234955+HaileB65@users.noreply.github.com>
2023-07-26 20:07:08 -0500

    Created Record Room app and repository

---
+++ src/main/java/Record_Room/RecordRoomApplication.java
@@ -0,0 +1,14 @@
+package Record_Room;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecordRoomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecordRoomApplication.class, args);
+		System.out.println("Welcome to Record Room");
+	}
+
+}
---
+++ src/test/java/Record_Room/RecordRoomApplicationTests.java
@@ -0,0 +1,13 @@
+package Record_Room;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecordRoomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
